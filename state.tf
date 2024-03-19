af984298b52c30426aa77e5ef7ef5fdbecfc9c9a
chaitu <chaitus.ch09@gmail.com>
2024-03-19 14:25:53 -0700

    state file resources

--- state.tf
+++ state.tf
@@ -1,10 +1,17 @@
 terraform {
-  backend "s3" {
-    bucket = "ch-jaikeer1"
-    key    = "testing/state"
+
+
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "5.41.0"
+      }
+    }
+  }
+
+  provider "aws" {
     region =  "us-east-1"
   }
-}
 
-resource "null_resource" "dummy1" {}
-resource "null_resource" "dummy2" {}
+
+
